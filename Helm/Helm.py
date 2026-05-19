1a998e979668be3d477d627596f95c10569117e7
Ali Abbas <aa0504503@gmail.com>
2026-05-19 12:09:26 -0400

    run cmd

--- Helm/Helm.py
+++ Helm/Helm.py
@@ -6,3 +6,9 @@ from pathlib import Path
 class HelmReleaseManager:
     def __init__(self, namespace=None):
         self.namespace = namespace
+    def run_cmd(self, cmd):
+        full_cmd = ['helm'] + cmd
+        if self.namespace:
+            full_cmd.extend(['-n', self.namespace])
+        result = subprocess.run(full_cmd, capture_output=True, text=True)
+        return result.returncode, result.stdout, result.stderr
